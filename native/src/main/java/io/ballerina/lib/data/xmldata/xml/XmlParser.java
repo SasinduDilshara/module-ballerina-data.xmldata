b37c025502d9cf694ffde2affd4f5930f78494c0
Sasindu Alahakoon <dilsharasasindu@gmail.com>
2024-11-07 21:15:06 +0530

    Update sequence tests

--- native/src/main/java/io/ballerina/lib/data/xmldata/xml/XmlParser.java
+++ native/src/main/java/io/ballerina/lib/data/xmldata/xml/XmlParser.java
@@ -467,9 +467,6 @@ public class XmlParser {
         QualifiedNameMap<Boolean> siblings = xmlParserData.siblings;
         Stack<QualifiedNameMap<Boolean>> parents = xmlParserData.parents;
 
-        // TODO: Check the conflict with L482
-//        boolean isModelGroupCompleted = finalizedModelStackAndReturnTrueIfCompleted(xmlParserData, elemQName);
-
         if (siblings.contains(elemQName) && !siblings.get(elemQName)) {
             siblings.put(elemQName, true);
         }
@@ -479,14 +476,7 @@ public class XmlParser {
         }
 
         validateRequiredFields(xmlParserData);
-
         finalizedModelStackAndReturnTrueIfCompleted(xmlParserData, elemQName);
-
-        //TODO: Uncomment this
-//        if (!isModelGroupCompleted) {
-//            xmlParserData.currentNode = (BMap<BString, Object>) xmlParserData.nodesStack.pop();
-//        }
-
         xmlParserData.currentNode = (BMap<BString, Object>) xmlParserData.nodesStack.pop();
         popExpectedTypeStacks(xmlParserData);
         updateSiblingAndRootRecord(xmlParserData);
